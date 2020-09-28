80f77acfb25aec98f76bb50a056889654640e7ef
André Ferreira <andrefpires1@gmail.com>
2020-09-28 20:47:50 -0300

    Exercício 1 | Parte I

---
+++ exercises/block-7/7.2/exercise1-pt1.js
@@ -0,0 +1,13 @@
+// Defina a variável playerNumber como 16. Em seguida, use a variável para pesquisar
+// o nome do jogador e atribuí-lo a player.
+
+let testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+let playerNumber = 16;
+let player = testObj[playerNumber];
+
+console.log(player);
