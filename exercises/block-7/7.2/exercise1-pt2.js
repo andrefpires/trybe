c23cc47c9bf53b010e743f0240df827e28957e59
André Ferreira <andrefpires1@gmail.com>
2020-09-28 21:12:45 -0300

    Exercício 1 | Parte I

---
+++ exercises/block-7/7.2/exercise1-pt2.js
@@ -0,0 +1,27 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+let addTurno = (objeto, chave, valor) => {
+  objeto[chave] = valor;
+}
+
+addTurno(lesson2, 'turno', 'manhã');
+
+console.log(lesson2);
