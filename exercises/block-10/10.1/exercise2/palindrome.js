73af30de6686315f0e6d847e7e83e166b5283935
André Ferreira <andrefpires1@gmail.com>
2020-11-01 17:22:11 -0300

    Exercício 2 | palindrome

---
+++ exercises/block-10/10.1/exercise2/palindrome.js
@@ -0,0 +1,32 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+const palindrome = (str) => {
+  let response = 'Parameter must be a string';
+
+  if (typeof str === 'string') {
+    let arrStr = str.split('');
+    const counter = arrStr.length - 1;
+    let reverseArrStr = [];
+
+    for (i = counter; i >= 0; i -= 1) {
+      reverseArrStr.push(arrStr[i]);
+    };
+
+    if (arrStr.join('') === reverseArrStr.join('')) {
+      response = true;
+    } else {
+      response = false;
+    }
+  }
+
+  return response;
+}
+
+module.exports = palindrome;
