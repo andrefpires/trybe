26c7f585324b926ac71fa31cefdc216e6fa5db91
André Ferreira <andrefpires1@gmail.com>
2020-11-04 00:21:27 -0300

    test

--- exercises/block-7/7.2/exercise1-pt1.js
+++ exercises/block-7/7.2/exercise1-pt1.js
@@ -1,5 +1,5 @@
 // Defina a variável playerNumber como 16. Em seguida, use a variável para pesquisar
-// o nome do jogador e atribuí-lo a player.
+// o nome do jogador e atribuí-lo a player. test
 
 let testObj = {
   12: "Namath",
