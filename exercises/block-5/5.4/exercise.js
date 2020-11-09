25d27443c14495a38dd23c6925deaaf0a515e881
André Ferreira <andrefpires1@gmail.com>
2020-11-09 02:58:56 -0300

    Exercício | Mudar tamanho da fonte e mudança no CSS

--- exercises/block-5/5.4/exercise.js
+++ exercises/block-5/5.4/exercise.js
@@ -1,6 +1,8 @@
 const tagBody = document.querySelector('body');
+const classText = document.querySelector('.text');
 let buttonBackgroundColor = document.querySelector('.button-backgroundColor');
 let buttonFontColor = document.querySelector('.button-fontColor');
+let buttonFontSize = document.querySelector('.button-fontSize');
 
 function changeBackgroundColor() {
   const inputBackgroundColor = document.querySelector('.input-backgroundColor');
@@ -12,8 +14,16 @@ buttonBackgroundColor.addEventListener('click', changeBackgroundColor);
 
 function changeFontColor() {
   const inputFontColor = document.querySelector('.input-fontColor');
-  tagBody.style.color = inputFontColor.value;
+  classText.style.color = inputFontColor.value;
 }
 // fazer validação dos dados do input para changeFontColor
 
 buttonFontColor.addEventListener('click', changeFontColor);
+
+function changeFontSize() {
+  const inputFontSize = document.querySelector('.input-fontSize');
+  classText.style.fontSize = `${inputFontSize.value}px`;
+}
+// fazer validação dos dados do input para changeFontSize
+
+buttonFontSize.addEventListener('click', changeFontSize);
