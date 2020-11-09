d1b730632ed56a29707538f7322d73e896ae5a61
André Ferreira <andrefpires1@gmail.com>
2020-11-09 16:51:51 -0300

    Exercício | finished

--- exercises/block-5/5.4/exercise.js
+++ exercises/block-5/5.4/exercise.js
@@ -1,10 +1,12 @@
 const tagBody = document.querySelector('body');
 const classText = document.querySelector('.text');
-let buttonBackgroundColor = document.querySelector('.button-backgroundColor');
-let buttonFontColor = document.querySelector('.button-fontColor');
-let buttonFontSize = document.querySelector('.button-fontSize');
-let buttonlineHeight = document.querySelector('.button-lineHeight');
-let buttonfontFamily = document.querySelector('.button-fontFamily');
+const tagP = document.querySelector('p');
+const buttonBackgroundColor = document.querySelector('.button-backgroundColor');
+const buttonFontColor = document.querySelector('.button-fontColor');
+const buttonFontSize = document.querySelector('.button-fontSize');
+const buttonlineHeight = document.querySelector('.button-lineHeight');
+const buttonfontFamily = document.querySelector('.button-fontFamily');
+const buttonText = document.querySelector('.button-text');
 
 function changeBackgroundColor() {
   const inputBackgroundColor = document.querySelector('.input-backgroundColor');
@@ -17,7 +19,7 @@ buttonBackgroundColor.addEventListener('click', changeBackgroundColor);
 
 function changeFontColor() {
   const inputFontColor = document.querySelector('.input-fontColor');
-  classText.style.color = inputFontColor.value;
+  tagP.style.color = inputFontColor.value;
   localStorage.setItem('font-color', inputFontColor.value);
 }
 // fazer validação dos dados do input para changeFontColor
@@ -26,7 +28,7 @@ buttonFontColor.addEventListener('click', changeFontColor);
 
 function changeFontSize() {
   const inputFontSize = document.querySelector('.input-fontSize');
-  classText.style.fontSize = `${inputFontSize.value}px`;
+  tagP.style.fontSize = `${inputFontSize.value}px`;
   localStorage.setItem('font-size', `${inputFontSize.value}px`);
 }
 // fazer validação dos dados do input para changeFontSize
@@ -35,7 +37,7 @@ buttonFontSize.addEventListener('click', changeFontSize);
 
 function changeLineHeight() {
   const inputlineHeight = document.querySelector('.input-lineHeight');
-  classText.style.lineHeight = `${inputlineHeight.value}px`;
+  tagP.style.lineHeight = `${inputlineHeight.value}px`;
   localStorage.setItem('line-height', `${inputlineHeight.value}px`);
 }
 // fazer validação dos dados do input para changeLineHeight
@@ -44,8 +46,8 @@ buttonlineHeight.addEventListener('click', changeLineHeight);
 
 function changeFontFamily() {
   const inputFontFamily = document.querySelector('.input-fontFamily');
-  classText.style.fontFamily = inputFontFamily.value;
-  localStorage.setItem('font-Family', `${inputFontFamily.value}`);
+  tagP.style.fontFamily = inputFontFamily.value;
+  localStorage.setItem('font-Family', inputFontFamily.value);
 }
 // fazer validação dos dados do input para changeFontFamily
 
@@ -53,15 +55,21 @@ buttonfontFamily.addEventListener('click', changeFontFamily);
 
 function getItemsLocalStorage(){
   const fontFamily = localStorage.getItem('font-Family');
-  classText.style.fontFamily = fontFamily;
+  tagP.style.fontFamily = fontFamily;
   const lineHeight = localStorage.getItem('line-height');
-  classText.style.lineHeight = lineHeight;
+  tagP.style.lineHeight = lineHeight;
   const fontSize = localStorage.getItem('font-size');
-  classText.style.fontSize = fontSize;
+  tagP.style.fontSize = fontSize;
   const fontColor = localStorage.getItem('font-color');
-  classText.style.color = fontColor;
+  tagP.style.color = fontColor;
   const backgroundColor = localStorage.getItem('background-color');
   tagBody.style.backgroundColor = backgroundColor;
 }
 
 getItemsLocalStorage();
+
+function createText() {
+  tagP.innerText = classText.value;
+}
+
+buttonText.addEventListener('click', createText);
