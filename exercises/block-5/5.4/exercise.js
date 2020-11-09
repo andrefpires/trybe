ae04224b2769c8cc43b82470b07c4ca2fa51d902
André Ferreira <andrefpires1@gmail.com>
2020-11-09 05:50:57 -0300

    Exercício | Adicionando função que recupera os items do localStorage

--- exercises/block-5/5.4/exercise.js
+++ exercises/block-5/5.4/exercise.js
@@ -9,6 +9,7 @@ let buttonfontFamily = document.querySelector('.button-fontFamily');
 function changeBackgroundColor() {
   const inputBackgroundColor = document.querySelector('.input-backgroundColor');
   tagBody.style.backgroundColor = inputBackgroundColor.value;
+  localStorage.setItem('background-color', inputBackgroundColor.value);
 }
 // fazer validação dos dados do input para changeBackgroundColor
 
@@ -17,6 +18,7 @@ buttonBackgroundColor.addEventListener('click', changeBackgroundColor);
 function changeFontColor() {
   const inputFontColor = document.querySelector('.input-fontColor');
   classText.style.color = inputFontColor.value;
+  localStorage.setItem('font-color', inputFontColor.value);
 }
 // fazer validação dos dados do input para changeFontColor
 
@@ -25,6 +27,7 @@ buttonFontColor.addEventListener('click', changeFontColor);
 function changeFontSize() {
   const inputFontSize = document.querySelector('.input-fontSize');
   classText.style.fontSize = `${inputFontSize.value}px`;
+  localStorage.setItem('font-size', `${inputFontSize.value}px`);
 }
 // fazer validação dos dados do input para changeFontSize
 
@@ -33,6 +36,7 @@ buttonFontSize.addEventListener('click', changeFontSize);
 function changeLineHeight() {
   const inputlineHeight = document.querySelector('.input-lineHeight');
   classText.style.lineHeight = `${inputlineHeight.value}px`;
+  localStorage.setItem('line-height', `${inputlineHeight.value}px`);
 }
 // fazer validação dos dados do input para changeLineHeight
 
@@ -41,7 +45,23 @@ buttonlineHeight.addEventListener('click', changeLineHeight);
 function changeFontFamily() {
   const inputFontFamily = document.querySelector('.input-fontFamily');
   classText.style.fontFamily = inputFontFamily.value;
+  localStorage.setItem('font-Family', `${inputFontFamily.value}`);
 }
-// fazer validação dos dados do input para changeLineHeight
+// fazer validação dos dados do input para changeFontFamily
 
 buttonfontFamily.addEventListener('click', changeFontFamily);
+
+function getItemsLocalStorage(){
+  const fontFamily = localStorage.getItem('font-Family');
+  classText.style.fontFamily = fontFamily;
+  const lineHeight = localStorage.getItem('line-height');
+  classText.style.lineHeight = lineHeight;
+  const fontSize = localStorage.getItem('font-size');
+  classText.style.fontSize = fontSize;
+  const fontColor = localStorage.getItem('font-color');
+  classText.style.color = fontColor;
+  const backgroundColor = localStorage.getItem('background-color');
+  tagBody.style.backgroundColor = backgroundColor;
+}
+
+getItemsLocalStorage();
