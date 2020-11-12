49502af00eb48585f6724b0ea86257a0877df130
André Ferreira <andrefpires1@gmail.com>
2020-11-12 19:16:12 -0300

    Exercício 2

--- exercises/block-10/10.2/exercises.test.js
+++ exercises/block-10/10.2/exercises.test.js
@@ -24,24 +24,24 @@ describe('Exercise 1', () => {
 // IDs existem.
 
 describe('Exercise 2', () => {
-    const users = {
-      4: { name: 'Mark' },
-      5: { name: 'Paul' }
-    };
-        
-    const findUserById = (id) => {
-      return new Promise((resolve, reject) => {
-          if (users[id]) {
-            return resolve(users[id]);
-          }
-        
-          return reject({ error: 'User with ' + id + ' not found.' });
-      });
-    }
-        
-    const getUserName = (userID) => {
-      return findUserById(userID).then(user => user.name);
-    }
+  const users = {
+    4: { name: 'Mark' },
+    5: { name: 'Paul' }
+  };
+
+  const findUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        if (users[id]) {
+          return resolve(users[id]);
+        }
+      
+        return reject({ error: 'User with ' + id + ' not found.' });
+    });
+  }
+      
+  const getUserName = (userID) => {
+    return findUserById(userID).then(user => user.name);
+  }
 
   it('the function returns the expected name by passing the id', async () => {
     expect.assertions(1);
