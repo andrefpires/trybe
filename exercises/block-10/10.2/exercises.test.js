65981e2d4cbdefe71adb4619de3b701f28ec7c48
André Ferreira <andrefpires1@gmail.com>
2020-11-12 23:04:19 -0300

    Exercício 5 - Correção seguindo o gabarito da Trybe

--- exercises/block-10/10.2/exercises.test.js
+++ exercises/block-10/10.2/exercises.test.js
@@ -124,12 +124,12 @@ describe('Exercício 5', () => {
 // Para este exercício, tente adivinhar a saída dos console.log dos testes abaixo sem executá-los, e veja
 // se compreendeu bem o funcionamento do beforeEach e do afterEach.
 
-  beforeEach(() => console.log('1 - beforeEach')); // 1°
-  afterEach(() => console.log('1 - afterEach')); // 3°
+  beforeEach(() => console.log('1 - beforeEach')); // 1° 4°
+  afterEach(() => console.log('1 - afterEach')); // 3° 8°
 
   test('', () => console.log('1 - test')); // 2°
 
-  describe('Scoped / Nested block', () => { // 4°
+  describe('Scoped / Nested block', () => {
     beforeEach(() => console.log('2 - beforeEach')); // 5°
     afterEach(() => console.log('2 - afterEach')); // 7°
 
