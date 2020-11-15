a9cca50ba5f1f7d55a6c34e36c580449aee23d19
André Ferreira <andrefpires1@gmail.com>
2020-11-15 15:22:49 -0300

    Mudando estrutura dos testes e adicionando forEach

--- exercises/block-10/10.3/exercises.test.js
+++ exercises/block-10/10.3/exercises.test.js
@@ -2,36 +2,42 @@ describe('Exercícios', () => {
   const exercises = require('./exercises');
   jest.mock('./exercises');
 
-  it('Exercício 1: função generateNumbersUpToTen', () => {
-    // 1. Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para
-    // essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual
-    // seu retorno e quantas vezes foi chamada.
-    exercises.generateNumbersUpToTen();
-    expect(exercises.generateNumbersUpToTen).toHaveBeenCalled();
-    exercises.generateNumbersUpToTen.mockReturnValueOnce(10);
-    expect(exercises.generateNumbersUpToTen()).toBe(10);
-    expect(exercises.generateNumbersUpToTen).toHaveBeenCalledTimes(2);
-  });
-
-  it('Exercício 2: Função divisionOfRandomNumbers', () => {
-    // 2. Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que
-    // deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve
-    // ocorrer uma única vez. Faça os testes necessários.
+  beforeEach( () => {
     const number1 = exercises.generateNumbersUpToTen();
     const number2 = exercises.generateNumbersUpToTen();
-    exercises.divisionOfRandomNumbers.mockReturnValue(10);
-    expect(typeof exercises.divisionOfRandomNumbers(number1, number2)).toBe('number');
-    expect(exercises.divisionOfRandomNumbers(number1, number2)).toBe(10);
-    exercises.divisionOfRandomNumbers(number1, number2);
-    expect(exercises.divisionOfRandomNumbers).toHaveBeenCalled();
-    expect(exercises.divisionOfRandomNumbers).toHaveBeenCalledTimes(3);
   });
 
-  // it('', () => {
-  //   // 3. Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que
-  //   // receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua
-  //   // implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
+  describe('Exercício 1', () => {
+    it('Função generateNumbersUpToTen', () => {
+      // 1. Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para
+      // essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual
+      // seu retorno e quantas vezes foi chamada.
+      expect(exercises.generateNumbersUpToTen).toHaveBeenCalled();
+      exercises.generateNumbersUpToTen.mockReturnValueOnce(10);
+      expect(exercises.generateNumbersUpToTen()).toBe(10);
+      expect(exercises.generateNumbersUpToTen).toHaveBeenCalledTimes(3);
+    });
+  });
 
-
-  // });
+  describe('Exercício 2', () => {
+    it('Função divisionOfRandomNumbers', () => {
+      // 2. Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que
+      // deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve
+      // ocorrer uma única vez. Faça os testes necessários.
+      const number1 = exercises.generateNumbersUpToTen();
+      const number2 = exercises.generateNumbersUpToTen();
+      exercises.divisionOfRandomNumbers.mockReturnValue(10);
+      expect(typeof exercises.divisionOfRandomNumbers(number1, number2)).toBe('number');
+      expect(exercises.divisionOfRandomNumbers(number1, number2)).toBe(10);
+      expect(exercises.divisionOfRandomNumbers).toHaveBeenCalled();
+      expect(exercises.divisionOfRandomNumbers).toHaveBeenCalledTimes(2);
+    });
+  });
 });
+
+//   it('', () => {
+//     // 3. Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que
+//     // receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua
+//     // implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
+
+//   });
