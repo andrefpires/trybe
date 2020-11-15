84bcc9caf12ffe41ed5acd396a7dc0b75b55d92b
André Ferreira <andrefpires1@gmail.com>
2020-11-15 16:59:55 -0300

    Exercício 3

--- exercises/block-10/10.3/exercises.js
+++ exercises/block-10/10.3/exercises.js
@@ -7,11 +7,17 @@ const divisionOfRandomNumbers = (number1, number2) => {
   return number1 / number2;
 };
 
-// const multiplicationOfRandomNumbers = () => {
+const multiplicationOfRandomNumbers = (number1, number2, number3) => {
+  return number1 * number2 * number3;
+}
 
-// }
+const doubleReturns = (number) => {
+  return number * 2;
+}
 
 module.exports = {
   generateNumbersUpToTen,
   divisionOfRandomNumbers,
+  multiplicationOfRandomNumbers,
+  doubleReturns,
 };
--- exercises/block-10/10.3/exercises.test.js
+++ exercises/block-10/10.3/exercises.test.js
@@ -2,9 +2,14 @@ describe('Exercícios', () => {
   const exercises = require('./exercises');
   jest.mock('./exercises');
 
+  let number1 = 0;
+  let number2 = 0;
+  let number3 = 0;
+
   beforeEach( () => {
-    const number1 = exercises.generateNumbersUpToTen();
-    const number2 = exercises.generateNumbersUpToTen();
+    number1 = exercises.generateNumbersUpToTen();
+    number2 = exercises.generateNumbersUpToTen();
+    number3 = exercises.generateNumbersUpToTen();
   });
 
   describe('Exercício 1', () => {
@@ -15,7 +20,7 @@ describe('Exercícios', () => {
       expect(exercises.generateNumbersUpToTen).toHaveBeenCalled();
       exercises.generateNumbersUpToTen.mockReturnValueOnce(10);
       expect(exercises.generateNumbersUpToTen()).toBe(10);
-      expect(exercises.generateNumbersUpToTen).toHaveBeenCalledTimes(3);
+      expect(exercises.generateNumbersUpToTen).toHaveBeenCalledTimes(4);
     });
   });
 
@@ -24,8 +29,6 @@ describe('Exercícios', () => {
       // 2. Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que
       // deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve
       // ocorrer uma única vez. Faça os testes necessários.
-      const number1 = exercises.generateNumbersUpToTen();
-      const number2 = exercises.generateNumbersUpToTen();
       exercises.divisionOfRandomNumbers.mockReturnValue(10);
       expect(typeof exercises.divisionOfRandomNumbers(number1, number2)).toBe('number');
       expect(exercises.divisionOfRandomNumbers(number1, number2)).toBe(10);
@@ -33,11 +36,23 @@ describe('Exercícios', () => {
       expect(exercises.divisionOfRandomNumbers).toHaveBeenCalledTimes(2);
     });
   });
+  describe('Exercício 3', () => {
+    it('Funções multiplicationOfRandomNumbers e doubleReturns', () => {
+    // 3. Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que
+    // receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua
+    // implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
+    exercises.multiplicationOfRandomNumbers();
+    expect(exercises.multiplicationOfRandomNumbers).toHaveBeenCalled();
+    exercises.multiplicationOfRandomNumbers.mockReturnValueOnce(20).mockReturnValue(10);
+    expect(exercises.multiplicationOfRandomNumbers(number1, number2, number3)).toBe(20);
+    expect(typeof exercises.multiplicationOfRandomNumbers(number1, number2, number3)).toBe('number');
+    expect(exercises.multiplicationOfRandomNumbers).toHaveBeenCalledTimes(3);
+    exercises.multiplicationOfRandomNumbers.mockReset();
+    exercises.doubleReturns.mockReturnValueOnce(60).mockReturnValueOnce(30);
+    expect(exercises.doubleReturns(number1)).toBe(60);
+    expect(typeof exercises.doubleReturns(number1)).toBe('number');
+    expect(exercises.doubleReturns).toHaveBeenCalled();
+    expect(exercises.doubleReturns).toHaveBeenCalledTimes(2);
+    });
+  });
 });
-
-//   it('', () => {
-//     // 3. Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que
-//     // receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua
-//     // implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
-
-//   });
