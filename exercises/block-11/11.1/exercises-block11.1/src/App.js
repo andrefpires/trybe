efb1bbb4d28961955b266dfd0e2e8692113f93d9
André Ferreira <andrefpires1@gmail.com>
2020-11-19 17:43:38 -0300

    Exercise 3 | freeCodeCamp 1

--- exercises/block-11/11.1/exercises-block11.1/src/App.js
+++ exercises/block-11/11.1/exercises-block11.1/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 const arrTasks = ['Arrumar cama', 'Sair pra passear com o cachorro', 'Reunião de sprint', 'Tomar remédio'];
 
 const tasks = () => arrTasks.map((element) => <li>{element}</li>)
+const title = <h1 className='titles'>Lista de compras</h1>
+const JSX = <h1 className ='titles'>Hello JSX!</h1>
 
 function App() {
   return (
     <div className='app'>
-      <h1 className='title'>Lista de compras</h1>
+      {title}
       <ul className='list'>
         {tasks()}
       </ul>
+      {JSX}
     </div>
   );
 }
