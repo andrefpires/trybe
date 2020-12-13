ca4f48dd694dd5bc3074fee22d0030307a244a5f
André Ferreira <andrefpires1@gmail.com>
2020-12-12 22:47:07 -0300

    Arrumando imports

--- modulo-2/block-12/movieCardLibraryStateful/src/App.js
+++ modulo-2/block-12/movieCardLibraryStateful/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import movies from './data';
 import './App.css';
-
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import MovieList from './components/MovieList/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={movies}/>
     </div>
   );
 }
--- modulo-2/block-12/movieCardLibraryStateful/src/components/MovieList/MovieList.jsx
+++ modulo-2/block-12/movieCardLibraryStateful/src/components/MovieList/MovieList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import MovieCard from './MovieCard';
+import MovieCard from '../MovieCard/MovieCard';
 
 class MovieList extends React.Component {
   render() {
