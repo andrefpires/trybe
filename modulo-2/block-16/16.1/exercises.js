57e319c239d8214223d6d7a191fd9b3c9ee7e655
André Ferreira <andrefpires1@gmail.com>
2021-01-19 15:18:08 -0300

    Exercício 10 (segunda opção para resolução)

--- modulo-2/block-16/16.1/exercises.js
+++ modulo-2/block-16/16.1/exercises.js
@@ -177,7 +177,7 @@ const immutableReducer = (state = todos, action) => {
   switch(action.type) {
     case ADD_TO_DO:
       // Don't mutate state here or the tests will fail
-      return [...state, action.todo]
+      return state.concat(action.todo);
     default:
       return state;
   }
