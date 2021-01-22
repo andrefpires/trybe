dc9f0370ef1345b25dcdb57d2d60fa5906869fca
André Ferreira <andrefpires1@gmail.com>
2021-01-22 13:59:55 -0300

    Redux do componente cars funcionando

--- modulo-2/block-16/16.2/exercise3/src/Cars.jsx
+++ modulo-2/block-16/16.2/exercise3/src/Cars.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { moveCar } from './redux/actionCreators';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
-export default function Cars({
-  redCar, blueCar, yellowCar,
+function Cars({
+  redCar, blueCar, yellowCar, moveCar
 }) {
   return (
     <div>
       <div>
         <img className={redCar ? 'car-right' : 'car-left'} src={carRed} alt="red car" />
-        <button type="button">Move</button>
+        <button onClick={() => moveCar('red', !redCar)} type="button">Move</button>
       </div>
       <div>
         <img className={blueCar ? 'car-right' : 'car-left'} src={carBlue} alt="blue car" />
-        <button type="button">Move</button>
+        <button onClick={() => moveCar('blue', !blueCar)} type="button">Move</button>
       </div>
       <div>
         <img className={yellowCar ? 'car-right' : 'car-left'} src={carYellow} alt="yellow car" />
-        <button type="button">Move</button>
+        <button onClick={() => moveCar('yellow', !yellowCar)} type="button">Move</button>
       </div>
     </div>
   );
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  moveCar: (car, side) => dispatch(moveCar(car, side)),
+});
+
+const mapStateToProps = (state) => ({
+  redCar: state.cars.red,
+  yellowCar: state.cars.yellow,
+  blueCar: state.cars.blue,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cars);
--- modulo-2/block-16/16.2/exercise3/src/redux/index.js
+++ modulo-2/block-16/16.2/exercise3/src/redux/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
+import cars from './reducers/cars';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(cars, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
